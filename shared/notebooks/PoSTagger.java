7e803d3243efc4956eb2b68026367c231a6253da
mani <sadhak001@gmail.com>
2019-11-26 23:55:43 +0000

    Adding execution step for Chunker to the valohai.yaml script

--- shared/notebooks/PoSTagger.java
+++ shared/notebooks/PoSTagger.java
@@ -7,28 +7,28 @@ import java.util.Arrays;
 
 public class PoSTagger {
     public static void main(String [] args) throws Exception {
-    System.out.println("[Started...]");
-    try (InputStream modelIn = new FileInputStream("en-pos-maxent.bin")) {
-        POSModel model = new POSModel(modelIn);
-        POSTaggerME tagger = new POSTaggerME(model);
+        System.out.println("[Started...]");
+        try (InputStream modelIn = new FileInputStream("en-pos-maxent.bin")) {
+            POSModel model = new POSModel(modelIn);
+            POSTaggerME tagger = new POSTaggerME(model);
 
-        // The sentence has to be split into words and passed to the POS Tagger function
-        String sentence[] = new String[]{"Most", "large", "cities", "in", "the", "US", "had",
-                                 "morning", "and", "afternoon", "newspapers", "."};
-        System.out.println("Sentence: " + Arrays.toString(sentence));
-        String tags[] = tagger.tag(sentence);
-        System.out.println(Arrays.toString(tags));
-        System.out.println();
-        
-        System.out.println("Probabilities of tags: ");
-        double tagProbabilities[] = tagger.probs();
-        Arrays.stream(tagProbabilities).forEach(System.out::println);
-        System.out.println();
-        
-        System.out.println("Tags as sequences (contains probabilities: ");
-        Sequence topSequences[] = tagger.topKSequences(sentence);
-        System.out.println(Arrays.toString(topSequences));
-    }
-    System.out.println("[...Finished]");
+            // The sentence has to be split into words and passed to the POS Tagger function
+            String sentence[] = new String[]{"Most", "large", "cities", "in", "the", "US", "had",
+                                     "morning", "and", "afternoon", "newspapers", "."};
+            System.out.println("Sentence: " + Arrays.toString(sentence));
+            String tags[] = tagger.tag(sentence);
+            System.out.println(Arrays.toString(tags));
+            System.out.println();
+            
+            System.out.println("Probabilities of tags: ");
+            double tagProbabilities[] = tagger.probs();
+            Arrays.stream(tagProbabilities).forEach(System.out::println);
+            System.out.println();
+            
+            System.out.println("Tags as sequences (contains probabilities: ");
+            Sequence topSequences[] = tagger.topKSequences(sentence);
+            System.out.println(Arrays.toString(topSequences));
+        }
+        System.out.println("[...Finished]");
     }
 }
--- shared/notebooks/watch-execution.sh
+++ shared/notebooks/watch-execution.sh
@@ -8,3 +8,5 @@ EXEC_COUNTER=${1:-}
 echo "Watching counter ${EXEC_COUNTER}"
 timeout 5 vh --valohai-token ${VALOHAI_TOKEN}   \
    exec watch ${EXEC_COUNTER}
+echo "Stopped watching counter ${EXEC_COUNTER}"
+echo "Re-run to continue to watch or run the './show-final-result.sh ${EXEC_COUNTER}' to see the final outcome"
