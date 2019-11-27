cea8af2ebd7fbbd50d07102d07de3cfc69eb9a01
mani <sadhak001@gmail.com>
2019-11-27 00:06:25 +0000

    Adding execution step for Parser to the valohai.yaml script

---
+++ shared/notebooks/Parser.java
@@ -0,0 +1,27 @@
+import java.io.InputStream;
+import java.io.FileInputStream;
+import opennlp.tools.parser.Parse;
+import opennlp.tools.parser.Parser;
+import opennlp.tools.parser.ParserModel;
+import opennlp.tools.parser.ParserFactory;
+import opennlp.tools.cmdline.parser.ParserTool; 
+import opennlp.tools.util.Sequence;
+import java.util.Arrays;
+
+public class Parser {
+    public static void main(String [] args) throws Exception {
+      System.out.println("[Started...]");
+      try (InputStream modelIn = new FileInputStream("../shared/en-parser-chunking.bin")){
+
+        ParserModel model = new ParserModel(modelIn);
+        Parser parser = ParserFactory.create(model);
+
+        String sentence = "The quick brown fox jumps over the lazy dog.";
+        Parse topParses[] = ParserTool.parseLine(sentence, parser, 1);
+        
+        System.out.println("Sentence: " + sentence + "\n");
+        Arrays.stream(topParses).forEach(System.out::println);
+      }
+      System.out.println("[...Finished]");
+    }
+}
